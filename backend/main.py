6e379ec9ccf0167fb732a8a89239e2f43259a493
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-07-17 14:01:11 -0400

    add fastapi logic for post request for image up

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,53 @@ from fastapi.responses import JSONResponse
 import uvicorn
 from pathlib import Path
 import os
+
+app = FastAPI(title="MTG AI Vision", version="0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+@app.get("/")
+async def root():
+    return {"message: MTG AI Vision"}
+
+@app.post("/upload-card")
+async def upload_card(file: UploadFile = File(...)):
+    """Upload and process card image"""
+
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="File type not image.")
+    
+    file_path = UPLOAD_DIR / file.filename
+
+    try:
+        with open(file_path, "wb") as buffer:
+            content = await file.read()
+            buffer.write(content)
+
+            return {
+                "message": "Image uploaded sucessfully",
+                "filename": file.filename,
+                "size": len(content)
+            }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
+    
+    finally:
+        if file_path.exists():
+            file_path.unlink()
+
+@app.get("/health")
+async def health_check():
+    return{"status": "healthy"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
