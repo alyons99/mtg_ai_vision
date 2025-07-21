98bc800a2982d715419ce26dd4c33de3697acd66
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-07-21 10:42:25 -0400

    Create model outlines

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field, create_engine, Session
+from datetime import datetime
+from typing import Optional
+import os
+
+class CardSearch():
+    pass
+
+class User():
+    pass
+
+class Favorite():
+    pass
