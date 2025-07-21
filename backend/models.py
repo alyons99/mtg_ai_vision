02e0daf505f133517134e04a99c94dea2318c638
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-07-21 10:50:54 -0400

    Update models.py with DB session

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field, create_engine, Session
 from datetime import datetime
 from typing import Optional
 import os
+from dotenv import load_dotenv
 
 class CardSearch():
     pass
@@ -11,3 +12,15 @@ class User():
 
 class Favorite():
     pass
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
